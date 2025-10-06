5c96063234d7bbe6015642ebc84684e57f312320
asgharkapk <162083661+asgharkapk@users.noreply.github.com>
2025-10-06 16:54:44 +0330

    Update readme.bash

--- backup/readme.bash
+++ backup/readme.bash
@@ -1,3 +1,39 @@
+      - name: 📝 Generate ERROR⚠️ LIST
+        run: |
+          echo "## NOT Converted Configs" > Error.md
+          echo "" >> Error.md
+          echo "|File Line Count|File URL|" >> Error.md
+          echo "|-|-|" >> Error.md
+
+          USER=$(echo "${{ github.repository }}" | cut -d'/' -f1)
+          REPO=$(echo "${{ github.repository }}" | cut -d'/' -f2)
+
+          find output_configs -type f -name "*.yaml" | sort | while read file; do
+            # شمارش خطوط فایل
+            LINE_COUNT=$(wc -l < "$file")
+            # echo "📋 شمارش خطوط فایل: $file → $LINE_COUNT"
+            if [ "$LINE_COUNT" -lt 5 ]; then
+              REL_PATH=$(echo "$file" | sed 's|^output_configs/||')
+              echo "|[${LINE_COUNT}]|https://github.com/${USER}/${REPO}/blob/main/output_configs/${REL_PATH}|" >> Error.md
+            fi
+          done
+
+      - name: 📝 Generate Output LIST
+        run: |
+          echo "## Exported Configs" > Output.list
+          echo "" >> Output.list
+          USER=$(echo "${{ github.repository }}" | cut -d'/' -f1)
+          REPO=$(echo "${{ github.repository }}" | cut -d'/' -f2)
+
+          find output_configs -type f -name "*.yaml" | sort | while read file; do
+            # شمارش خطوط فایل
+            LINE_COUNT=$(wc -l < "$file")
+            if [ "$LINE_COUNT" -gt 10 ]; then
+              REL_PATH=$(echo "$file" | sed 's|^output_configs/||')
+              echo "[${LINE_COUNT}]${REL_PATH}|https://raw.githubusercontent.com/${USER}/${REPO}/refs/heads/main/output_configs/${REL_PATH}" >> Output.list
+            fi
+          done
+
       - name: 📑 Generate README.md
         run: |
           echo "# لیست پراکسی های استخراج شده از کالکتور های گیتهاب" > README.md
